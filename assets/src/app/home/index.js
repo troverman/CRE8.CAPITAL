c7e5a30d0051f05362849eecd2af82d6b1e33d42
Trevor Overman <troverman@gmail.com>
2017-01-23 07:08:40 -0500

    creative

--- assets/src/app/home/index.js
+++ assets/src/app/home/index.js
@@ -22,119 +22,6 @@ angular.module( 'investing.home', [
 	titleService.setTitle('investingfor');
 	$scope.currentUser = config.currentUser;
 
-	var ww, wh, renderer, scene, camera, cloud;
-
-	var opt = {
-	  radius: 20
-	};
-
-	function init() {
-
-	  ww = window.innerWidth;
-	  wh = window.innerHeight;
-
-	  renderer = new THREE.WebGLRenderer({
-	    canvas: document.getElementById("scene"),
-	    antialias: true
-	  });
-	  renderer.setSize(ww, wh);
-	  renderer.setClearColor(0x808080);
-
-	  scene = new THREE.Scene();
-
-	  camera = new THREE.PerspectiveCamera(50, ww / wh, 0.1, 10000);
-	  camera.position.set(250, 250, 250);
-	  camera.lookAt(new THREE.Vector3(0, 0, 0));
-	  scene.add(camera);
-
-
-	  createDots();
-	  requestAnimationFrame(render);
-	  window.addEventListener("resize", onWindowResize);
-	}
-
-
-	function onWindowResize(){
-	  ww = window.innerWidth;
-	  wh = window.innerHeight;
-	  
-	  camera.aspect = ww/wh;
-	  camera.updateProjectionMatrix();
-
-	  renderer.setSize(ww,wh);
-	}
-
-	function createDots() {
-	  var geometry = new THREE.Geometry();
-	  var x, y, z, vertex;
-	  var i = 0;
-	  for (var x = -5; x < 5; x++) {
-	    for (var y = -5; y < 5; y++) {
-	      for (var z = -5; z < 5; z++) {
-	        vertex = new THREE.Vector3(
-	          x * opt.radius,
-	          y * opt.radius,
-	          z * opt.radius
-	        );
-	        // var theta = (i/density) * (mouse.x*100);
-	        // var delta = (i / density - 0.5) * Math.PI * (mouse.y);
-	        // var x = 200 * Math.cos(delta) * Math.cos(theta); 
-	        // var y = 200 * Math.cos(delta) * Math.sin(theta);
-	        // var z = 200 * Math.sin(delta);
-	        
-	        vertex.index = Math.abs(x) + Math.abs(y) + Math.abs(z);
-	        geometry.vertices.push(vertex);
-	        i++;
-	      }
-	    }
-	  }
-	  geometry.vertices = geometry.vertices.sort(sortVertices);
-	  var animation = new TimelineMax({
-	    repeat: -1,
-	    repeatDelay: 0.5
-	  });
-	  animation.staggerTo(geometry.vertices, 2.5, {
-	    cycle: {
-	      x: function(i) {
-	        return -geometry.vertices[i].z - opt.radius
-	      },
-	      y: function(i) {
-	        return geometry.vertices[i].y
-	      },
-	      z: function(i) {
-	        return geometry.vertices[i].x
-	      }
-	    },
-	    ease: SlowMo.ease.config(0.5, 0.5, false)
-	  }, 0.0007)
-	  var material = new THREE.PointsMaterial({
-	    color: 0xECF0F1,
-	    size: 2,
-	    sizeAttenuation: false
-	  });
-	  cloud = new THREE.Points(geometry, material);
-
-	  scene.add(cloud);
-	}
-
-	function sortVertices(a, b) {
-	  if (a.index > b.index) {
-	    return 0;
-	  } else {
-	    return -1;
-	  }
-	}
-
-	function render(a) {
-	  requestAnimationFrame(render);
-
-	  cloud.geometry.verticesNeedUpdate = true;
-
-	  renderer.render(scene, camera);
-	}
-
-	init();
-
 	$scope.options = {
         chart: {
             type: 'candlestickBarChart',
@@ -248,8 +135,6 @@ angular.module( 'investing.home', [
         {"date": 15952, "open": 164.43, "high": 166.03, "low": 164.13, "close": 165.75, "volume": 97304000, "adjusted": 165.75},
         {"date": 15953, "open": 165.85, "high": 166.4, "low": 165.73, "close": 165.96, "volume": 62930500, "adjusted": 165.96}
     ]}];
-
-    //console.log(currencyData);
 
     var color = d3.scale.category20()
     $scope.directedOptions = {
--- assets/src/app/intro/index.js
+++ assets/src/app/intro/index.js
@@ -2,4 +2,119 @@ angular.module( 'investing.intro', [
 ])
 
 .controller( 'IntroCtrl', function IntroController( $scope, $state, config ) {
+
+	console.log('ok')
+	var ww, wh, renderer, scene, camera, cloud;
+
+	var opt = {
+	  radius: 20
+	};
+
+	function init() {
+
+	  ww = window.innerWidth;
+	  wh = window.innerHeight;
+
+	  renderer = new THREE.WebGLRenderer({
+	    canvas: document.getElementById("scene"),
+	    antialias: true
+	  });
+	  renderer.setSize(ww, wh);
+	  renderer.setClearColor(0x4e4e4e);
+
+	  scene = new THREE.Scene();
+
+	  camera = new THREE.PerspectiveCamera(50, ww / wh, 0.1, 10000);
+	  camera.position.set(250, 250, 250);
+	  camera.lookAt(new THREE.Vector3(0, 0, 0));
+	  scene.add(camera);
+
+
+	  createDots();
+	  requestAnimationFrame(render);
+	  window.addEventListener("resize", onWindowResize);
+	}
+
+
+	function onWindowResize(){
+	  ww = window.innerWidth;
+	  wh = window.innerHeight;
+	  
+	  camera.aspect = ww/wh;
+	  camera.updateProjectionMatrix();
+
+	  renderer.setSize(ww,wh);
+	}
+
+	function createDots() {
+	  var geometry = new THREE.Geometry();
+	  var x, y, z, vertex;
+	  var i = 0;
+	  for (var x = -5; x < 5; x++) {
+	    for (var y = -5; y < 5; y++) {
+	      for (var z = -5; z < 5; z++) {
+	        vertex = new THREE.Vector3(
+	          x * opt.radius,
+	          y * opt.radius,
+	          z * opt.radius
+	        );
+	        // var theta = (i/density) * (mouse.x*100);
+	        // var delta = (i / density - 0.5) * Math.PI * (mouse.y);
+	        // var x = 200 * Math.cos(delta) * Math.cos(theta); 
+	        // var y = 200 * Math.cos(delta) * Math.sin(theta);
+	        // var z = 200 * Math.sin(delta);
+	        
+	        vertex.index = Math.abs(x) + Math.abs(y) + Math.abs(z);
+	        geometry.vertices.push(vertex);
+	        i++;
+	      }
+	    }
+	  }
+	  geometry.vertices = geometry.vertices.sort(sortVertices);
+	  var animation = new TimelineMax({
+	    repeat: -1,
+	    repeatDelay: 0.5
+	  });
+	  animation.staggerTo(geometry.vertices, 2.5, {
+	    cycle: {
+	      x: function(i) {
+	        return -geometry.vertices[i].z - opt.radius
+	      },
+	      y: function(i) {
+	        return geometry.vertices[i].y
+	      },
+	      z: function(i) {
+	        return geometry.vertices[i].x
+	      }
+	    },
+	    ease: SlowMo.ease.config(0.5, 0.5, false)
+	  }, 0.0007)
+	  var material = new THREE.PointsMaterial({
+	    color: 0xECF0F1,
+	    size: 2,
+	    sizeAttenuation: false
+	  });
+	  cloud = new THREE.Points(geometry, material);
+
+	  scene.add(cloud);
+	}
+
+	function sortVertices(a, b) {
+	  if (a.index > b.index) {
+	    return 0;
+	  } else {
+	    return -1;
+	  }
+	}
+
+	function render(a) {
+	  requestAnimationFrame(render);
+
+	  cloud.geometry.verticesNeedUpdate = true;
+
+	  renderer.render(scene, camera);
+	}
+
+	init();
+
 });
