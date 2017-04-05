4870ad6114c912e194bbb35af970ce6acd1ae643
Trevor Overman <troverman@gmail.com>
2017-04-05 07:17:00 -0400

    https

--- config/env/production.js
+++ config/env/production.js
@@ -1,18 +1,14 @@
 /**
  * Production environment settings
  *
- * This file can include shared settings for a production environment,
- * such as API keys or remote database passwords.  If you're using
- * a version control solution for your Sails app, this file will
- * be committed to your repository unless you add it to your .gitignore
- * file.  If your repository will be publicly viewable, don't add
- * any private information to this file!
- *
  */
 
 module.exports = {
 
-  hookTimeout: 10000000
+  hookTimeout: 10000000,
+  policies: {
+    '*': ['enforceSsl', 'passport']
+  },
 
 
   /***************************************************************************
