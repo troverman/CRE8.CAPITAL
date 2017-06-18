d8cdb5233ec0b2ae4ce054f82476ea855bd3f2f4
Trevor Overman <troverman@gmail.com>
2017-06-18 05:03:04 -0400

    ?

--- api/services/intervalService.js
+++ api/services/intervalService.js
@@ -332,8 +332,6 @@ module.exports.intervalService = function(){
 		var trainer = new Trainer(initNetwork)
 		//console.log(initNetwork)
 		var networkJson = initNetwork.toJSON();
-
-
 		NeuralNetwork.create({title:'1 min ' + tradingPairs[x], predictionTime:'60000', assetPair: tradingPairs[x], asset1:tradingPairs[x][0], asset2:tradingPairs[x][1], networkJson:networkJson }).then(function(){console.log('HI')})
 		NeuralNetwork.create({title:'5 min ' + tradingPairs[x], predictionTime:'300000', assetPair: tradingPairs[x], asset1:tradingPairs[x][0], asset2:tradingPairs[x][1], networkJson:networkJson }).then(function(){console.log('HI')})
 		NeuralNetwork.create({title:'30 min ' + tradingPairs[x], predictionTime:'1800000', assetPair: tradingPairs[x], asset1:tradingPairs[x][0], asset2:tradingPairs[x][1], networkJson:networkJson }).then(function(){console.log('HI')})
@@ -341,8 +339,6 @@ module.exports.intervalService = function(){
 		NeuralNetwork.create({title:'6 hr ' + tradingPairs[x], predictionTime:'21600000', assetPair: tradingPairs[x], asset1:tradingPairs[x][0], asset2:tradingPairs[x][1], networkJson:networkJson }).then(function(){console.log('HI')})
 		NeuralNetwork.create({title:'12 hr ' + tradingPairs[x], predictionTime:'43200000', assetPair: tradingPairs[x], asset1:tradingPairs[x][0], asset2:tradingPairs[x][1], networkJson:networkJson }).then(function(){console.log('HI')})
 		NeuralNetwork.create({title:'24 hr ' + tradingPairs[x], predictionTime:'86400000', assetPair: tradingPairs[x], asset1:tradingPairs[x][0], asset2:tradingPairs[x][1], networkJson:networkJson }).then(function(){console.log('HI')})
-
-
 	};*/
 
 
@@ -393,48 +389,37 @@ module.exports.intervalService = function(){
 	/*
 	var myNetwork = new Architect.Perceptron(2, 4, 3, 2);
 	var trainer = new Trainer(myNetwork);
-
 	var myNetwork1 = new Architect.Perceptron(2, 4, 3, 2);
 	var trainer1 = new Trainer(myNetwork1);
-
 	var myNetwork2 = new Architect.Perceptron(2, 4, 3, 2);
 	var trainer2 = new Trainer(myNetwork2);
-
 	var myNetwork3 = new Architect.Perceptron(2, 4, 3, 2);
 	var trainer3 = new Trainer(myNetwork3);
-
 	var myNetwork4 = new Architect.Perceptron(2, 4, 3, 2);
 	var trainer4 = new Trainer(myNetwork4);
-
 	//portfolio weight is 0-1 btc to usd
 	////make trade? 
 	var budgetNetwork = new Architect.Perceptron(2, 4, 3, 2);
 	var budgetTrainer = new Trainer(budgetNetwork);
-
 	//neuralNet(50000,80000);
 	//neuralNet(30000,60000*5);
 	//neuralNet(30000/5,60000);
-
 	//1 min, 6 seconds(x10)
 	//1 min to train, wait 1 min, 1 min to train
 	//new prediction every 3 min
 	setInterval(neuralNet.bind(null, 6000, 60000, myNetwork, trainer), 60000);
-
 	//5 min, 30 sec(x10)
 	//5 min to train, wait 5 min, 5 min to train
 	//new prediction every 15min
 	setInterval(neuralNet.bind(null, 30000, 300000, myNetwork1, trainer1), 300000);
-
 	//30 min, 180 sec(x10)
 	//30 min to train, wait 30 min, 30 min to train
 	//new prediction every 90min
 	setInterval(neuralNet.bind(null, 180000, 1800000, myNetwork2, trainer2), 1800000);
-
 	//90 min, 540 sec(x10)
 	//90 min to train, wait 90 min, 90 min to train
 	//new prediction every 270min
 	setInterval(neuralNet.bind(null, 540000, 5400000, myNetwork3, trainer3), 5400000);
-
 	//720 min, 4320 sec(x10)
 	//720 min to train, wait 720 min, 720 min to train
 	//new prediction every 36hrs
