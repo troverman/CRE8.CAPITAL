ddfdd39662f1267cbf12350812904950693c0baa
Trevor Overman <troverman@gmail.com>
2017-12-09 20:21:03 -0500

    1

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,9 +13,9 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  console.log(sails.services.dataservice, dataService);
+  console.log(sails.services.dataservice, marketService);
 
-  intervalService.intervalService();
+  //intervalService.intervalService();
   sails.services.passport.loadStrategies();
   sails.services.emailservice.loadTemplates().then(function(){
   	cb();
