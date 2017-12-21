aa30fd196ccc16eb659ddea65a11431db02861d7
Trevor Overman <troverman@gmail.com>
2017-12-21 15:37:58 -0500

    1234

--- assets/src/app/markets/index.js
+++ assets/src/app/markets/index.js
@@ -126,7 +126,7 @@ angular.module( 'investing.markets', [
 
 
     //do an analysis of % change...
-    $scope.dataMap = {}
+   /* $scope.dataMap = {}
     $scope.seletetData = function (asset1, asset2, delta){
         $rootScope.stateIsLoading = true;
         $scope.selectedPair = [asset1, asset2];
@@ -156,7 +156,7 @@ angular.module( 'investing.markets', [
                     $scope.marketGraphDataRender = [$scope.marketGraphData];
                 }
         }
-    });
+    });*/
 
 
 }]);
