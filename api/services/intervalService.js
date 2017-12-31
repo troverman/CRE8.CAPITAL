60310128dbb3c19e45b05bd53b782be305f0b387
Trevor Overman <troverman@gmail.com>
2017-12-31 15:30:19 -0500

    common

--- api/services/intervalService.js
+++ api/services/intervalService.js
@@ -585,7 +585,7 @@ module.exports.intervalService = function(){
     	}
     }); */
 
-	/*timer(dataService.tickerREST.bind(null, 1000), 1000);//second
+	timer(dataService.tickerREST.bind(null, 1000), 1000);//second
 	timer(dataService.tickerREST.bind(null, 1000*5), 1000*5);//5 seconds
 	timer(dataService.tickerREST.bind(null, 1000*5*6), 1000*5*6);//30 seconds
 	timer(dataService.tickerREST.bind(null, 1000*5*12), 1000*5*12);//60 seconds
@@ -596,7 +596,7 @@ module.exports.intervalService = function(){
 	timer(dataService.tickerREST.bind(null, 1000*5*12*5*6*2*2*2), 1000*5*12*5*6*2*2*2);//4hr
 	timer(dataService.tickerREST.bind(null, 1000*5*12*5*6*2*2*3), 1000*5*12*5*6*2*2*3);//6hr
 	timer(dataService.tickerREST.bind(null, 1000*5*12*5*6*2*2*3*2), 1000*5*12*5*6*2*2*3*2);//12hr
-	timer(dataService.tickerREST.bind(null, 1000*5*12*5*6*2*2*3*2*2), 1000*5*12*5*6*2*2*3*2*2);//24hr*/
+	timer(dataService.tickerREST.bind(null, 1000*5*12*5*6*2*2*3*2*2), 1000*5*12*5*6*2*2*3*2*2);//24hr
 
 	//cull the data.. 
 	timer(dataService.cullData.bind(null, '1000', 30*60*1000), 100000);//second
