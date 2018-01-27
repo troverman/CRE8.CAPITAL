8f674a1fa02e055b871667abd24961d3e4f293b2
Trevor Overman <troverman@gmail.com>
2018-01-27 13:29:55 -0500

    1234

--- api/controllers/AnalysisController.js
+++ api/controllers/AnalysisController.js
@@ -545,11 +545,182 @@ module.exports = {
 
 	},
 
-	//TODO: DO THIS L88R
+	//only using TSF atm
 	//IF THIS IS PROFITABLE.. TURN IT ON!
+	//TODO: restructure to object strcutre vs. reliying on indexing 
 	portfolioSolvePDF: function(req, res) {
 
-	},
+		function getPdf(pairData){
+			for (y in pairData){
+				//build prediction array.
+				if (y > 0){
+					var predictionData = pairData.slice(0, y);
+					var tsfPredictionData = [];
+					for (var z; z<=y; y++){
+						tsfPredictionData.push(dataService.getTSF(predictionData, periodArray[z]));
+					}
+					Q.all(tsfPredictionData)
+					.then(function(data){
+						//large list of predictions.. (not pdf yet.)
+						//theoretitically pdf will have +8.8 indicators +1 complexity
 
+						//return obj here for predictions 
+						//at each time at each pair
+			
+						var sortedData = data.sort(function(a,b) {return (a < b) ? 1 : ((b < a) ? -1 : 0);}); 
+						var lastPrice = pairData[pairData.length-1].price;
+					
+						//INIT PDF: might need to save pdfs as prediction db
+						//populate appropiately ~ normal dist around 0? liner relation..
+						var pdfMap = {};
+						for (var i=1000; i>=-1000; i--){
+							pdfMap[i/1000] = 0.60811/Math.pow(i, 2);
+						}
+						pdfMap[0] = pdfMap[0.001];
+
+						var pdfData = sortedData.map(function(obj){return (obj - lastPrice)/lastPrice});
+						for (x in pdfData){
+							pdfMap[parseFloat(testData[x].toFixed(3))] += 0.1
+						}
+
+						//console.log(data);
+						//return data;
+						return pdfMap
+
+					});
+
+				}
+			}
+		};
+
+		var exchangeMap = [];
+		var promises = [];
+		var currentPortfolio = {BTC:req.query.btc}
+		var limit = req.query.limit;
+		var delta = req.query.delta;
+		var portfolioSet = [];
+		var orderSet = [];
+		tradingPairs = tradingPairs.filter(function(obj){
+	        if (obj.split('/')[1]=='BTC'){return obj}
+	    });
+		tradingPairs.forEach(function(tradingPair, index){
+		    var promise = getData(limit, delta, tradingPair);
+		    promises.push(promise);
+		});
+		Q.all(promises)
+		.then(function(data){
+			exchangeMap = data;
+
+			//async
+			var pdfArray = [];
+			async.eachSeries(exchangeMap, function (pairData, nextPair){
+				getPdf(pairData).then(function(pdfMap){
+					pdfArray.push(pdfMap)
+					process.nextTick(nextPair);
+				});
+			},
+			function(){
+
+				for (y in exchangeMap[0]){
+					var timeArray = [];
+					var predictionArray = [];
+					for (x in exchangeMap){
+						timeArray.push(exchangeMap[x][y]);
+						predictionArray.push(pdfArray[x][y]);
+						if (!hasUndefined(timeArray)){
+							if(!currentPortfolio[exchangeMap[x][y].asset1]){currentPortfolio[exchangeMap[x][y].asset1]=0}
+							if(!currentPortfolio[exchangeMap[x][y].asset2]){currentPortfolio[exchangeMap[x][y].asset2]=0}
+						}
+					}
+
+					var pdfAnalysis = [];
+					for (x in predictionArray){
+
+						//find most 'attractive' pdf
+						//pdf analysis
+						var pdfMap = predictionArray[x];
+						var pdfSum = 0;
+						var totalPositiveProbability = 0;
+						var totalNegativeProbability = 0;
+						var allPostiveProbability = true;
+
+						for (z in Object.keys(pdfMap)){
+							var key = Object.keys(pdfMap)[z];
+							pdfSum += pdfMap[key];
+							if (pdfMap[key] > 0){totalPositiveProbability += pdfMap[key];}
+							if (pdfMap[key] < 0){totalNegativeProbability += pdfMap[key];allPostiveProbability=false}
+						}
+						pdfAnalysis.push({pdfSum:pdfSum, totalPositiveProbability:totalPositiveProbability, totalNegativeProbability:totalNegativeProbability, allPostiveProbability:allPostiveProbability});
+
+					}
+
+					//find index of most 'attractive'
+					//sort by probability score ~ for multipick 
+					var positiveProbabilityIndex = pdfAnalysis.map(function(obj){return obj.allPostiveProbability}).indexOf(true);
+
+					//sort by totalPositiveProbability - totalNegativeProbability
+					//need to find index..
+					//var sortMap = pdfAnalysis.map(function(obj){return obj.totalPositiveProbability + obj.totalNegativeProbability});
+					//this may be redundant vs relativity +1 start complexity
+					var sortMap = pdfAnalysis.sort(function(a,b) {
+						return (a.totalPositiveProbability + a.totalNegativeProbability < b.totalPositiveProbability + b.totalNegativeProbability) ? 1 : ((b.totalPositiveProbability + b.totalNegativeProbability < a.totalPositiveProbability + a.totalNegativeProbability) ? -1 : 0);
+					}); 
+
+					//TODO: move away from index.
+					var pickOrderIndex = [];
+					for (x in sortMap){
+						//totalPositiveProbability is not neccesarily unique 
+						pickOrderIndex.push(pdfAnalysis.map(function(obj){return obj.totalPositiveProbability}).indexOf(sortMap[x].totalPositiveProbability));
+					}
+
+					//TODO: add positiveProbabilityIndex to pickOrderIndex, reduce.
+
+					//MULTIPICK -- prob best to cap from 3-7
+					var cap = 4;
+					var cappedPickIndex = pickOrderIndex.slice(0, cap);
+
+					//TODO: relativity? 
+					var totalPositiveChange = 0;
+					for (x in pdfAnalysis){
+						totalPositiveChange += pdfAnalysis[x].totalPositiveProbability;
+					}
+
+
+					//STRAT
+					//go to btc
+					for (n in Object.keys(currentPortfolio)){
+						var pairIndex = timeArray.map(function(obj){return obj.asset2}).indexOf(Object.keys(currentPortfolio)[n]);
+						if (pairIndex != -1){
+							if (Object.keys(currentPortfolio)[n] != 'BTC' && currentPortfolio[Object.keys(currentPortfolio)[n]] != 0){
+
+								currentPortfolio['BTC'] += currentPortfolio[Object.keys(currentPortfolio)[n]]*parseFloat(timeArray[pairIndex].price);
+								orderSet.push({asset1: timeArray[pairIndex].asset2, asset2:timeArray[pairIndex].asset1, price:1/parseFloat(timeArray[pairIndex].price), amount:currentPortfolio[Object.keys(currentPortfolio)[n]], createdAt:timeArray[pairIndex].createdAt})
+								currentPortfolio[Object.keys(currentPortfolio)[n]] = 0;
+								portfolioSet.push(clone(currentPortfolio));
+
+							}
+						}
+					};
+
+					//MULTIPICK
+					for (n in cappedPickIndex){
+						
+						var asset1Amount = currentPortfolio[timeArray[cappedPickIndex[n]].asset1];
+						var relativeAmount = asset1Amount*pdfAnalysis[cappedPickIndex[n]].totalPositiveProbability/totalPositiveChange;
+
+						currentPortfolio[timeArray[cappedPickIndex[n]].asset2] += relativeAmount/timeArray[cappedPickIndex[n]].price;
+						currentPortfolio['BTC'] = currentPortfolio['BTC'] - relativeAmount;
+						
+						orderSet.push({asset1: timeArray[cappedPickIndex[n]].asset1, asset2:timeArray[cappedPickIndex[n]].asset2, price:parseFloat(timeArray[cappedPickIndex[n]].price), amount:relativeAmount, createdAt:timeArray[cappedPickIndex[n]].createdAt})
+						
+						portfolioSet.push(clone(currentPortfolio));
+
+					}
+
+				}
+
+			});
+		});
+	},
 	
 };
